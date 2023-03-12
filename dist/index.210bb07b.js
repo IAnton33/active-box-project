442bf969cd5a02624dcc55c837f266ee2db85249
ivanovaa <3260870@tut.by>
2023-03-12 19:20:42 +0300

    dist

---
+++ dist/index.210bb07b.js
@@ -0,0 +1,2 @@
+const sidebar=document.querySelector(".sidebar"),onBurgerClick=()=>{sidebar.classList.toggle("sidebar_active")};
+//# sourceMappingURL=index.210bb07b.js.map
---
+++ dist/index.59b1264b.js
@@ -0,0 +1,6 @@
+const sidebar = document.querySelector(".sidebar");
+const onBurgerClick = ()=>{
+    sidebar.classList.toggle("sidebar_active");
+};
+
+//# sourceMappingURL=index.59b1264b.js.map
